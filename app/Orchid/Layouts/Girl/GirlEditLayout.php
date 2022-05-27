cc7efe009cbcb3950b87759fe9b6d12dc161a6f8
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2022-05-27 11:15:32 +0300

    update

--- app/Orchid/Layouts/Girl/GirlEditLayout.php
+++ app/Orchid/Layouts/Girl/GirlEditLayout.php
@@ -5,35 +5,47 @@ declare(strict_types=1);
 namespace App\Orchid\Layouts\Girl;
 
 use Orchid\Screen\Field;
+use Orchid\Screen\Fields\CheckBox;
+use Orchid\Screen\Fields\Group;
 use Orchid\Screen\Fields\Input;
 use Orchid\Screen\Layouts\Rows;
 use Orchid\Support\Facades\Layout;
 
 class GirlEditLayout extends Rows
 {
-    /**
-     * Views.
-     *
-     * @return Field[]
-     */
+
     public $title = 'Услуги';
+
+
+    public function ddddd () {
+
+
+
+
+        return [
+            CheckBox::make('readonly_checkbox')
+                    ->title('Readonly Checkbox')
+                    ->placeholder('Remember me'),
+            CheckBox::make('readonly_checkbox')
+                    ->title('Readonly Checkbox')
+                    ->placeholder('Remember me'),
+            CheckBox::make('readonly_checkbox')
+                    ->title('Readonly Checkbox')
+                    ->placeholder('Remember me'),
+            CheckBox::make('readonly_checkbox')
+                    ->title('Readonly Checkbox')
+                    ->placeholder('Remember me'),
+        ];
+    }
+
+
     public function fields(): array
     {
 
         return  [
-
-            Input::make('user.name')
-                ->type('text')
-                ->max(255)
-                ->required()
-                ->title(__('Name'))
-                ->placeholder(__('Name')),
-
-            Input::make('user.email')
-                ->type('email')
-                ->required()
-                ->title(__('Email'))
-                ->placeholder(__('Email')),
+            Group::make(
+                $this->ddddd()
+                )
         ];
     }
 }
