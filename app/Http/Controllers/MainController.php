96e61064a97b0d9de0e5f0c413c0e8ce5c85c457
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2022-05-27 18:13:35 +0300

    update

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Profile;
 use App\Models\Models\Service;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class MainController extends Controller {
     public function __construct() {
@@ -17,9 +18,14 @@ class MainController extends Controller {
         $test = Profile::find(1);
         $test2 = $test->fields->find(1);
         $test3 = $test2->servicesField->services;
+       $test = Service::all() ->load('ServicesField.field')
         dd($test3);
         */
-        $test = Service::all()->load('ServicesField.field');
+        $test =  DB::table('services')
+            ->join('services_field', 'services.id', '=', 'services_field.service_id')
+            ->join('fields', 'services_field.id', '=', 'fields.field_id')
+            ->select('services.*', 'services_field.name', 'fields.description')
+            ->get();
         dd($test);
         return view( 'in_dev' );
     }
