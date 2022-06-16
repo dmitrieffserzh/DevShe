013b51df3fc03537797b30c5369073c9f75c36cf
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2022-06-16 17:31:01 +0300

    update

--- app/Orchid/Screens/Rate/RateEditScreen.php
+++ app/Orchid/Screens/Rate/RateEditScreen.php
@@ -2,47 +2,57 @@
 
 namespace App\Orchid\Screens\Rate;
 
+use App\Models\Rate;
+use Orchid\Screen\Fields\Input;
+use Orchid\Screen\Fields\TextArea;
+use Orchid\Support\Facades\Layout;
+use Orchid\Screen\Actions\Button;
+use Orchid\Screen\Fields\Group;
 use Orchid\Screen\Screen;
 
-class RateEditScreen extends Screen
-{
-    /**
-     * Query data.
-     *
-     * @return array
-     */
-    public function query(): iterable
-    {
-        return [];
+class RateEditScreen extends Screen {
+
+    public function query( Rate $rate ): iterable {
+        return [
+            'rate' => $rate
+        ];
     }
 
-    /**
-     * Display header name.
-     *
-     * @return string|null
-     */
-    public function name(): ?string
-    {
+    public function name(): ?string {
         return 'RateEditScreen';
     }
 
-    /**
-     * Button commands.
-     *
-     * @return \Orchid\Screen\Action[]
-     */
-    public function commandBar(): iterable
-    {
-        return [];
+    public function commandBar(): iterable {
+        return [
+            Button::make( 'save' )->action( 'save' )->icon( 'plus' )
+        ];
     }
 
-    /**
-     * Views.
-     *
-     * @return \Orchid\Screen\Layout[]|string[]
-     */
-    public function layout(): iterable
-    {
-        return [];
+    public function layout(): iterable {
+        return [
+            Layout::columns( [
+                //Layout::rows( [
+                Group::make( [
+                    Input::make( 'name' )->title( 'Название' ),
+                    TextArea::make( 'description' )->title( 'Описание' )->placeholder( 'Введите описане тарифа' )
+                ] ),
+                Group::make( [
+                    TextArea::make( 'information' )->title( 'Пояснение' )->placeholder( 'Введите пояснение тарифа' )
+                ] ),
+                Group::make( [
+                    Input::make( 'plan_one_name' )->title( __( 'Name' ) ),
+                    Input::make( 'plan_one_price' )->title( __( 'Name' ) )
+                ] ),
+                Group::make( [
+                    Input::make( 'plan_two_name' )->title( __( 'Name' ) ),
+                    Input::make( 'plan_two_price' )->title( __( 'Name' ) )
+                ] ),
+                Group::make( [
+                    Input::make( 'plan_three_name' )->title( __( 'Name' ) ),
+                    Input::make( 'plan_three_price' )->title( __( 'Name' ) )
+                ] )
+                //])
+            ] )
+        ];
     }
 }
