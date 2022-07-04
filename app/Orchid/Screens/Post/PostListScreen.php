693f800c3847cdb95cbce6426744e8a2ad0f8e73
Sergey Dmitriev <dmitrieffserzh@yandex.ru>
2022-07-04 11:34:03 +0300

    update

--- app/Orchid/Screens/Post/PostListScreen.php
+++ app/Orchid/Screens/Post/PostListScreen.php
@@ -3,28 +3,25 @@
 namespace App\Orchid\Screens\Post;
 
 use App\Models\Post;
+use App\Orchid\Layouts\Post\PostListLayout;
 use Illuminate\Http\Request;
 use Orchid\Screen\Actions\Link;
 use Orchid\Screen\Screen;
 use Orchid\Support\Facades\Toast;
 
-class PostListScreen extends Screen
-{
+class PostListScreen extends Screen {
 
-    public function query(): iterable
-    {
+    public function query(): iterable {
         return [
-        //    'posts' => Post::filters()->defaultSort('id', 'desc')->paginate(),
+            'posts' => Post::filters()->defaultSort( 'id', 'desc' )->paginate(),
         ];
     }
 
-    public function name(): ?string
-    {
+    public function name(): ?string {
         return 'Статьи';
     }
 
-    public function commandBar(): iterable
-    {
+    public function commandBar(): iterable {
         return [
             Link::make( __( 'Add' ) )
                 ->icon( 'plus' )
@@ -32,30 +29,24 @@ class PostListScreen extends Screen
         ];
     }
 
-    public function layout(): iterable
-    {
+    public function layout(): iterable {
         return [
-
-
+            PostListLayout::class,
         ];
     }
 
+    public function status( Request $request ) {
+        $profile         = Post::findOrFail( $request->get( 'id' ) );
+        $profile->active = $request->get( 'status' );
+        if ( $profile->save() ) {
+            Toast::info( 'Статус статьи успешно изменен!' );
 
-    public function status(Request $request)
-    {
-        $profile = Post::findOrFail($request->get('id'));
-        $profile->active = $request->get('status');
-        if ($profile->save()) {
-            Toast::info('Статус статьи успешно изменен!');
-
-            return redirect()->route('platform.posts');
+            return redirect()->route( 'platform.posts' );
         }
     }
 
-    public function remove(Request $request): void
-    {
-        Post::findOrFail($request->get('id'))->delete();
-
-        Toast::info('Статья успешно удалена');
+    public function remove( Request $request ): void {
+        Post::findOrFail( $request->get( 'id' ) )->delete();
+        Toast::info( 'Статья успешно удалена' );
     }
 }
