772584df2c9d4d878230c0c78f48945430e228a7
dmitrieffserzh <dmitrieffserzh@yandex.ru>
2022-07-06 10:30:45 +0300

    update

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -5,11 +5,11 @@
 @section('content')
 
     {{--    <div class="content">--}}
-    <div class="profile">
-        <div class="profile__column">
+    <div class="profile-edit">
+        <div class="profile-edit__column">
             @include('profiles.aside')
         </div>
-        <div class="profile__column">
+        <div class="profile-edit__column">
         <!--<div class="profile__images">
                     <div class="images swiper">
                         <div class="swiper-wrapper">
@@ -32,12 +32,78 @@
                 </div>
             </div>
         </div>-->
+
+            <div class="description">
+                    <textarea name="profile[description]" id="" cols="30" rows="10"
+                              placeholder="Коротко о себе">{{ $profile->description }}</textarea>
+            </div>
         </div>
-        <div class="profile__column">
+        <div class="profile-edit__column">
+            <div class="information">
+                <div class="profile-edit__row">
+                    <input type="text" value="{{ $profile->name }}">
+                    <select name="profile[age]" id="" class="js-select">
+                        @foreach(Helpers::getGirlAge() as $key=>$value)
+                            <option value="{{ $key }}" @if($key == $profile->age) selected @endif>{{ $value }}</option>
+                        @endforeach
+                    </select>
+                    <input type="text" value="{{ $profile->height }}">
+                    <input type="text" value="{{ $profile->weight }}">
+                </div>
+
+                <div class="profile-edit__row">
+                    <select name="profile[appearance]" id="" class="js-select">
+                        @foreach(Helpers::getGirlAppearance() as $key=>$value)
+                            <option value="{{ $key }}"
+                                    @if($key == $profile->appearance) selected @endif>{{ $value }}</option>
+                        @endforeach
+                    </select>
+
+                    <select name="profile[breast_size]" id="" class="js-select">
+                        @foreach(Helpers::getGirlBreast() as $key=>$value)
+                            <option value="{{ $key }}"
+                                    @if($key == $profile->breast_size) selected @endif>{{ $value }}</option>
+                        @endforeach
+                    </select>
+
+                    <select name="profile[haircolor]" id="" class="js-select">
+                        @foreach(Helpers::getGirlHaircolor() as $key=>$value)
+                            <option value="{{ $key }}"
+                                    @if($key == $profile->haircolor) selected @endif>{{ $value }}</option>
+                        @endforeach
+                    </select>
+                </div>
+
+
+                <div class="profile-edit__row">
+                    <select name="profile[haircut]" id="" class="js-select">
+                        @foreach(Helpers::getGirlHaircut() as $key=>$value)
+                            <option value="{{ $key }}"
+                                    @if($key == $profile->haircut) selected @endif>{{ $value }}</option>
+                        @endforeach
+                    </select>
+
+                    <label for="breast-natural">
+                        Натуральная
+                        <input id="breast-natural" type="radio" name="profile[breast_type]" value="0"
+                               @if($profile->breast_type == 0) checked @endif>
+                    </label>
+                    <label for="breast-silicon">
+                        Силикон
+                        <input id="breast-silicon" type="radio" name="profile[breast_type]" value="1"
+                               @if($profile->breast_type == 1) checked @endif>
+                    </label>
+                </div>
+            </div>
+            <div class="contacts">
+                <input type="text" name="profile[phone]" value="{{ $profile->phone ?? '' }}">
+                <input type="text" name="profile[whatsapp]" value="{{ $profile->whatsapp ?? '' }}">
+                <input type="text" name="profile[telegram]" value="{{ $profile->telegram ?? ''}}">
+            </div>
+
             <div class="rates">
                 <div class="rates__title">
-                    <h2>Тарифы:</h2>
-                    <span/>Дата обновления: 25.04.2022</span>
+                    <h2>Введите данные для тарифов услуг</h2>
                 </div>
                 <div class="rates__list">
                     <div class="rate rate--day">
@@ -119,66 +185,12 @@
                     </div>
                 </div>
             </div>
-            <div class="information">
-                <input type="text" value="{{ $profile->height }}">
-                <input type="text" value="{{ $profile->weight }}">
-
-                <select name="profile[age]" id="">
-                    @foreach(Helpers::getGirlAge() as $key=>$value)
-                        <option value="{{ $key }}" @if($key == $profile->age) selected @endif>{{ $value }}</option>
-                    @endforeach
-                </select>
-
-                <select name="profile[appearance]" id="">
-                    @foreach(Helpers::getGirlAppearance() as $key=>$value)
-                        <option value="{{ $key }}"
-                                @if($key == $profile->appearance) selected @endif>{{ $value }}</option>
-                    @endforeach
-                </select>
-
-                <select name="profile[breast_size]" id="">
-                    @foreach(Helpers::getGirlBreast() as $key=>$value)
-                        <option value="{{ $key }}"
-                                @if($key == $profile->breast_size) selected @endif>{{ $value }}</option>
-                    @endforeach
-                </select>
-
-                <select name="profile[haircolor]" id="">
-                    @foreach(Helpers::getGirlHaircolor() as $key=>$value)
-                        <option value="{{ $key }}"
-                                @if($key == $profile->haircolor) selected @endif>{{ $value }}</option>
-                    @endforeach
-                </select>
-
-                <select name="profile[haircut]" id="">
-                    @foreach(Helpers::getGirlHaircut() as $key=>$value)
-                        <option value="{{ $key }}" @if($key == $profile->haircut) selected @endif>{{ $value }}</option>
-                    @endforeach
-                </select>
-                <label for="breast-natural">
-                    Натуральная
-                    <input id="breast-natural" type="radio" name="profile[breast_type]" value="0"
-                           @if($profile->breast_type == 0) checked @endif>
-                </label>
-                <label for="breast-silicon">
-                    Силикон
-                    <input id="breast-silicon" type="radio" name="profile[breast_type]" value="1"
-                           @if($profile->breast_type == 1) checked @endif>
-                </label>
-
-                <div class="description">
-                    <textarea name="profile[description]" id="" cols="30" rows="10"
-                              placeholder="Коротко о себе">{{ $profile->description }}</textarea>
-                </div>
-
-            </div>
-            <div class="contacts">
-                <input type="text" name="profile[phone]" value="{{ $profile->phone ?? '' }}">
-                <input type="text" name="profile[whatsapp]" value="{{ $profile->whatsapp ?? '' }}">
-                <input type="text" name="profile[telegram]" value="{{ $profile->telegram ?? ''}}">
+            <div class="input_radio">
+                <input id="express" type="checkbox">
+                <label for="express">Есть экспресс</label>
             </div>
         </div>
-        <div class="profile__column">
+        <div class="profile-edit__column">
             <div class="services">
                 <h2 class="services__title">Предпочтения:</h2>
                 @foreach($services as $block)
@@ -186,10 +198,12 @@
                         <div class="block__title">{{ $block['title'] }}</div>
                         <div class="block__content">
                             @foreach($block['services'] as $service)
+                                @php( $id = rand(1000,99999))
                                 <div class="service">
-                                    <label class="service__title">
-                                        <input type="checkbox" {{$service['check'] ? ' checked':'' }}>
-                                        {{ $service['name'] }}</label>
+                                    <div class="input_radio">
+                                        <input id="{{ $id }}" type="checkbox" {{$service['check'] ? ' checked':'' }}>
+                                        <label for="{{ $id }}">{{ $service['name'] }}</label>
+                                    </div>
                                     <div class="service__description">
                                         <textarea name="" id="">{{ $service['description'] }}</textarea>
                                     </div>
